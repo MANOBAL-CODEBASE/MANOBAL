db80f16ac3d45774567fd8a89b3d2b112b064c9f
Nitish Kumar <nk9990207902@gmail.com>
2024-11-27 12:36:09 +0530

    resolve changes in frontend & backend

--- frontend/app/(auth)/sign-in.jsx
+++ frontend/app/(auth)/sign-in.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
-import React from 'react';
-import { tailwind } from 'tailwindcss-react-native';
-=======
 import React, { useState } from 'react';
 import { router } from 'expo-router';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { TextInput, TouchableOpacity, Alert } from 'react-native';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
@@ -22,19 +17,8 @@ const LoginScreen = () => {
     // Here you can integrate your authentication logic (e.g., API call to check user credentials)
     Alert.alert('Login Successful');
   };
->>>>>>> fd11d6d932cf106ea0a2fed38566d35a83bbce4a
 
   return (
-<<<<<<< HEAD
-    <View style={tailwind('flex-1 justify-center items-center')}>
-      <Text style={tailwind('text-lg font-bold m-3')}>SignIn Page</Text>
-      <Text style={tailwind('text-base mb-2')}>Email</Text>
-      <Text style={tailwind('text-base mb-2')}>Password</Text>
-      <Text style={tailwind('text-sm')}>Don't have an account?</Text>
-      <Link href="/sign-up" style={tailwind('text-blue-500 underline')}>
-        Sign Up
-      </Link>
-=======
     <View style={styles.container}>
       <Text style={styles.title}>Sign In</Text>
       
@@ -59,7 +43,6 @@ const LoginScreen = () => {
       </TouchableOpacity>
 
       <Text onPress={()=>{router.push('/sign-up')}} style={styles.footerText}>Don't have an account? Sign up</Text>
->>>>>>> fd11d6d932cf106ea0a2fed38566d35a83bbce4a
     </View>
   );
 };
