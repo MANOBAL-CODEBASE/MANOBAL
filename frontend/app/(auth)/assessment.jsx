5b3290f864002ab21b950660f82df399ab0eabca
Nitish Kumar <nk9990207902@gmail.com>
2024-11-29 11:32:58 +0530

    setup AsyncStorage

--- frontend/app/(auth)/assessment.jsx
+++ frontend/app/(auth)/assessment.jsx
@@ -1,10 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import authService from '../services/authServices';
 
 const Assessment = () => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const retrievedToken = await authService.getToken();
+        setToken(retrievedToken);
+      } catch (error) {
+        console.error('Error fetching token:', error);
+      }
+    };
+
+    fetchToken();
+  }, []); // Run only once when the component mounts
+
   return (
     <View style={styles.center}>
       <Text>Assessment</Text>
+      <Text>{token ? token : 'No token found'}</Text>
     </View>
   );
 };
--- frontend/app/services/authServices.js
+++ frontend/app/services/authServices.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const BASE_URL = 'http://192.168.1.38:4000';
 
@@ -17,10 +18,30 @@ const authService = {
       const url = `${BASE_URL}/api/auth/login`;
       const response = await axios.post(url, userData);
       const token = response.data.token;
-      // await AsyncStorage.setItem('manobal-token', token);
+      if (typeof token !== 'string') {
+        token = JSON.stringify(token);
+      }
+      await AsyncStorage.clear();
+
+      await AsyncStorage.setItem('manobal', token);
       return response.data;
     } catch (error) {
       throw error;
+    }
+  },
+  getToken: async () => {
+    try {
+      const token = await AsyncStorage.getItem('manobal');
+      if (token !== null) {
+        console.log('Retrieved token:', token);
+        return token; // Successfully retrieved token
+      } else {
+        console.log('Token not found');
+        return null; // Token does not exist
+      }
+    } catch (error) {
+      console.error('Error retrieving token:', error);
+      return null; // Return null if there’s an error
     }
   },
 };
